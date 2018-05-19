70a7784317659d65fed0ce2e157065c344077de3
Raman <Raman@raman-personal>
2018-05-19 14:50:50 -0700

    I dont know what im doing

---
+++ TownBuilder/src/TownBuilder.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.math.*;
+import java.io.*;
+
+public class TownBuilder {
+	
+	int population = 0;
+	int employed = 0;
+	int[] rsc = {}; 
+	String[] rsc_names = {};
+
+	public static void generateResources()
+	{
+		
+	}
+	
+	
+}
