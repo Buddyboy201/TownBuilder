ec5b69c42d023292de833683277a66a92f68a173
Raman <Raman@raman-personal>
2018-05-21 08:52:53 -0700

    Random commit

--- TownBuilder/src/TownBuilder.java
+++ TownBuilder/src/TownBuilder.java
@@ -5,14 +5,17 @@ import java.io.*;
 public class TownBuilder {
 	
 	int population = 0;
-	int employed = 0;
-	int[] rsc = {}; 
-	String[] rsc_names = {};
-
-	public static void generateResources()
+	//int employed = 0;
+	int[] rsc = {0, 0, 0, 0, 0, 0, 0, 0, 0}; 
+	String[] rsc_names = {"power", "food", "water", "lumber", "bricks", "plastic", "coal", "oil", "electronic_parts"};
+	int[][] tile_map = new int[5][5];
+	String[] tile_map_rsc_names = {"food", "water", "lumber", "coal", "oil"};
+	
+	
+	public void generateResources(int[] tile)
 	{
-		
+		//food
+		double rand = Math.random();
+		tile[0] = rand*x;
 	}
-	
-	
 }
