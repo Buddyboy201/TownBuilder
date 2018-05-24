080f7b118f97c261a508ac7cadef8aa774bc3e2c
Raman <Raman@raman-personal>
2018-05-23 20:26:13 -0700

    Market commit 1

---
+++ TownBuilder/src/Item.java
@@ -0,0 +1,13 @@
+
+public class Item {
+	double curr_value;
+	String category;
+	String name;
+	
+	public Item(double curr_value, String category, String name)
+	{
+		this.curr_value = curr_value;
+		this.category = category;
+		this.name = name;
+	}
+}
--- TownBuilder/src/Market.java
+++ TownBuilder/src/Market.java
@@ -1,4 +1,12 @@
+import java.util.ArrayList;
 
+//categories: natural resources, food, construction materials, manufactured goods, tools
 public class Market {
-
+	ArrayList<Item> natural_rsc = new ArrayList<Item>();
+	ArrayList<Item> food = new ArrayList<Item>();
+	ArrayList<Item> construction_materials = new ArrayList<Item>();
+	ArrayList<Item> manufactured_goods = new ArrayList<Item>();
+	ArrayList<Item> tools = new ArrayList<Item>();
+	
+	
 }
